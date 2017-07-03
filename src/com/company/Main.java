a85088bf4013100ac9078e07a61be319627875f2
ike seika <xingyunri81@yahoo.co.jp>
2017-07-03 22:43:43 +0900

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        TestClass cc = new TestClass();
+        System.out.print("hello prj" + cc.getValue());
+    }
+}
