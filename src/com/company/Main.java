2ff39ad95aa893ceaf8f279a9cc1a6419bdccd93
ike seika <xingyunri81@yahoo.co.jp>
2017-10-22 15:20:02 +0900

    kkk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         TestClass cc = new TestClass();
-        // devlop commit
+        // devlop123 commit
         System.out.print("hello123" + cc.getValue());
     }
 }
