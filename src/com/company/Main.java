899cc264d184b052c4e0e71b750b3f19cd2b8527
ike seika <xingyunri81@yahoo.co.jp>
2017-10-22 14:44:45 +0900

    test commmit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         TestClass cc = new TestClass();
-        // devlop commit
-        // master commit
+        // Test commit
         System.out.print("hello123" + cc.getValue());
     }
 }
